2e8afd46d506af5c59a7a3863719f09cb11f71c2
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-06-05 13:11:36 +0100

    reduce test coverage to diagnose ePrimer3 issue

--- tests/test_eprimer3.py
+++ tests/test_eprimer3.py
@@ -112,7 +112,7 @@ class TestCommands(unittest.TestCase):
 
     def test_eprimer3_exe(self):
         """ePrimer3 executable exists and runs."""
-        cmd = "{0} --version".format(self.ep3_exe)
+        cmd = "{0} --help".format(self.ep3_exe)
         result = subprocess.run(cmd, shell=sys.platform != "win32",
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE,
