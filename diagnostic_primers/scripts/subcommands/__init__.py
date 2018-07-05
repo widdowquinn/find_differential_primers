e2c056617a5a134de1ac04b2a92a81ab323aef97
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2018-07-05 14:41:03 +0100

    add dedupe subcommand to remove duplicate primer sets from config files

--- diagnostic_primers/scripts/subcommands/__init__.py
+++ diagnostic_primers/scripts/subcommands/__init__.py
@@ -5,6 +5,7 @@ from .subcmd_config import subcmd_config
 from .subcmd_prodigal import subcmd_prodigal
 from .subcmd_eprimer3 import subcmd_eprimer3
 from .subcmd_primersearch import subcmd_primersearch
+from .subcmd_dedupe import subcmd_dedupe
 from .subcmd_blastscreen import subcmd_blastscreen
 from .subcmd_classify import subcmd_classify
 from .subcmd_extract import subcmd_extract
