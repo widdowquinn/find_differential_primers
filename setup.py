a7731495a7f7981a4a3c7ef4cfe8992aa3bf4c0a
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2018-12-06 15:54:44 +0000

    fix version regex quoting in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as dfh:
 # parse version from package/module without importing or evaluating the code
 with open("diagnostic_primers/__init__.py") as fh:
     for line in fh:
-        m = re.search(r"^__version__ = '(?P<version>[^']+)'$", line)
+        m = re.search(r'^__version__ = "(?P<version>[^"]+)"$', line)
         if m:
             version = m.group("version")
             break
